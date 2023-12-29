401e8dc59e40db14780d02f8cdad04ed824c5727
BullDeveloper2022 <151467701+BullDeveloper2022@users.noreply.github.com>
2023-12-29 11:19:00 +0800

    Update Aave.sol

--- Aave.sol
+++ Aave.sol
@@ -1,6 +1,22 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
+interface IPool {
+    function supply(address asset, uint256 amount, address onBehalfOf, uint16 referralCode) external;
+    function withdraw(address asset, uint256 amount, address to) external returns (uint256);
+}
+
+interface IERC20 {
+    function totalSupply() external view returns (uint256);
+    function balanceOf(address account) external view returns (uint256);
+    function transfer(address recipient, uint256 amount) external returns (bool);
+    function allowance(address owner, address spender) external view returns (uint256);
+    function approve(address spender, uint256 amount) external returns (bool);
+    function transferFrom(address sender, address recipient, uint256 amount) external returns (bool);
+    event Transfer(address indexed from, address indexed to, uint256 value);
+    event Approval(address indexed owner, address indexed spender, uint256 value);
+}
+
 contract Aave
 {
     constructor(){}
@@ -18,4 +34,6 @@ contract Aave
     function deposit(uint amount) external {}
     // Reinvest
     function depositInterestAndReward() external{}
+
+
 }
